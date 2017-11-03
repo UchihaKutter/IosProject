176b1fd065d3be123047811326bb97016648f13d
liyun11118@gmail.com <liyun11118@gmail.com>
2017-11-03 18:10:21 +0800

    dddd

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!jsldldlfkkfkfkffffggggddfdd
+          Welcome to React Native!jsldldlfkkfkfkffffggggddfddssss
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
