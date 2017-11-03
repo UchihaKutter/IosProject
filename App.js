df34baae395bd7372e46d3e014a610b7feee9b57
liluan <LanHaiTianWangDjq@lanhaitianwang-Dj.local>
2017-11-03 17:52:28 +0800

    dddd

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!jsldldlfkkfkfk
+          Welcome to React Native!jsldldlfkkfkfkffff
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
